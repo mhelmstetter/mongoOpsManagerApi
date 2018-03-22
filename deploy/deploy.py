1081910edbf4de83412a72d644a65bcc6b5310c5
mhelmstetter <mark@helmstetter.com>
2018-03-22 12:57:33 -0700

    add/rempve replica members

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -40,54 +40,32 @@ processTemplate = {
     }
 }
 
-monitoringUser = {
-    "initPwd" : "cf2ac21046052a0f61215dd86d94b708",
-    "db": "admin",
-    "user": "mms-monitoring-agent",
-    "roles": [
-        {
-            "role": "clusterMonitor",
-            "db": "admin"
-        }
-    ]
-}
+def get_automation_status():
+    response = requests.get(automationStatusEndpoint
+            ,auth=HTTPDigestAuth(args.username,args.apiKey), verify=False)
+    response.raise_for_status()
+    return response.json()
 
-backupUser =  {
-    "initPwd": "b386182320b2c23a7adcbcee7840b1d1",
-    "db": "admin",
-    "user": "mms-backup-agent",
-    "roles": [
-        {
-            "role": "clusterAdmin",
-            "db": "admin"
-        },
-        {
-            "role": "readAnyDatabase",
-            "db": "admin"
-        },
-        {
-            "role": "userAdminAnyDatabase",
-            "db": "admin"
-        },
-        {
-            "role": "readWrite",
-            "db": "local"
-        },
-        {
-            "role": "readWrite",
-            "db": "admin"
-        }
-    ]
-}
+def wait_for_goal_state():
+    count = 0
+    while True:
+        continue_to_wait = False
+        status = get_automation_status()
+        goal_version = status['goalVersion']
 
-# TODO - how do we get the matching version of the monitoring agent to Ops Manager?
-monitoringVersion = {
-            "directoryUrl": None,
-            "hostname": "",
-            "baseUrl": None,
-            "name": "5.4.3.361"
-        }
+        for process in status['processes']:
+            logging.info("Round: %s GoalVersion: %s Process: %s (%s) LastVersionAchieved: %s Plan: %s "
+                 % (count, goal_version, process['name'], process['hostname'], process['lastGoalVersionAchieved'], process['plan']))
 
+            if process['lastGoalVersionAchieved'] < goal_version:
+                continue_to_wait = True
+
+        if continue_to_wait:
+            time.sleep(5)
+        else:
+            logging.info("All processes in Goal State. GoalVersionAchieved: %s" % goal_version)
+            break
+        count += 1
 
 def getAutomationConfig():
     response = requests.get(automationConfigEndpoint
@@ -102,9 +80,6 @@ def printAutomationConfig():
     config = getAutomationConfig()
     configStr = json.dumps(config, indent=4)
     print(configStr)
-
-
-
 
 
 def addReplicaSet():
@@ -135,6 +110,126 @@ def addReplicaSet():
     print(configStr)
 
     __post_automation_config(new_config)
+    wait_for_goal_state()
+
+def addReplicaMember():
+    config = getAutomationConfig()
+    new_config = copy.deepcopy(config)
+
+    data = open( args.rsTemplate, 'r').read()
+    rsTemplate = json.loads(data)
+
+    hostPort = args.hostPort.split(':')
+    if len(hostPort) != 2:
+        print "ERROR Invalid hostPort %s, should be of the form mongohost1.foo.com:27017" % (hostPort)
+        sys.exit(2)
+
+    host = hostPort[0]
+    port = hostPort[1]
+
+    modifiedCount = 0
+    for item in list(new_config['processes']):
+        if item.get('hostname') == host and item.get('args2_6', {}).get('net', {}).get('port') == int(port):
+            modifiedCount += 1
+            processName = item.get('name')
+            print processName
+            break
+
+    if modifiedCount > 0:
+        print "ERROR Host %s already exists in automation config" % (args.hostPort)
+        sys.exit(1)
+
+
+    rsName = args.rsName
+    rsIds = []
+    rsCount = 0
+
+    for replicaSet in list(new_config['replicaSets']):
+        if replicaSet.get('_id') == rsName:
+            for member in list(replicaSet['members']):
+                rsIds.append(member.get('_id'))
+
+    print "Existing rsIds %s" % str(rsIds)
+
+    process_id = max(rsIds) + 1
+    rsMemberIndex = len(rsIds)
+    rsConfig = copy.deepcopy(rsTemplate)
+    rsConfig['_id'] = args.rsName
+
+    #TODO make this a function
+    process = copy.deepcopy(processTemplate)
+    process['hostname'] = host
+    process['name'] = args.rsName + "_" + str(process_id)
+    process['args2_6']['replication']['replSetName'] = args.rsName
+    new_config['processes'].append(process)
+
+    # TODO assumes template members are all the same,
+    # need to know template/details for the member we're adding
+    rsConfig['members'][rsMemberIndex]['_id'] = process_id
+    rsConfig['members'][rsMemberIndex]['host'] = args.rsName + "_" + str(process_id)
+
+    for replicaSet in list(new_config['replicaSets']):
+        if replicaSet.get('_id') == rsName:
+            replicaSet['members'].append(rsConfig['members'][rsMemberIndex])
+            print str(rsConfig['members'][rsMemberIndex])
+
+    configStr = json.dumps(new_config, indent=4)
+    print(configStr)
+    __post_automation_config(new_config)
+    wait_for_goal_state()
+
+
+
+def removeReplicaMember():
+    config = getAutomationConfig()
+    new_config = copy.deepcopy(config)
+
+    hostPort = args.hostPort.split(':')
+    if len(hostPort) != 2:
+        print "ERROR Invalid hostPort %s, should be of the form mongohost1.foo.com:27017" % (hostPort)
+        sys.exit(2)
+
+    host = hostPort[0]
+    port = hostPort[1]
+
+    modifiedCount = 0
+    procsList = list(new_config['processes'])
+    for idx,item in enumerate(procsList):
+        if item.get('hostname') == host and item.get('args2_6', {}).get('net', {}).get('port') == int(port):
+            modifiedCount += 1
+            processName = item.get('name')
+            print processName
+            procsList.pop(idx)
+            break
+    new_config['processes'] = procsList
+
+    rsNameId = processName.split('_')
+    rsName = rsNameId[0];
+    print rsName
+    rsMemberId = int(rsNameId[1]);
+
+    for replicaSet in list(new_config['replicaSets']):
+        if replicaSet.get('_id') == rsName:
+            rsList = list(replicaSet['members'])
+            for idx,member in enumerate(rsList):
+                if member.get('_id') == rsMemberId:
+                    modifiedCount += 1
+                    print str(member.get('_id')) + " --> " +  str(idx)
+                    rsList.pop(idx)
+            replicaSet['members'] = rsList
+
+    print str(rsList)
+
+    if modifiedCount >= 2:
+        print processName
+
+        configStr = json.dumps(new_config, indent=4)
+        print(configStr)
+        __post_automation_config(new_config)
+        wait_for_goal_state()
+    else:
+        print "WARNING No matching host(s) %s found in automation config" % (args.hostPort)
+        sys.exit(1)
 
 
 def __post_automation_config(automation_config):
@@ -187,6 +282,12 @@ actionsParser.add_argument("--printAutomationConfig",dest='action', action='stor
 actionsParser.add_argument("--addReplicaSet",dest='action', action='store_const'
         ,const=addReplicaSet
         ,help='Add new replica set')
+actionsParser.add_argument("--removeReplicaMember",dest='action', action='store_const'
+        ,const=removeReplicaMember
+        ,help='Remove member from existing replica set')
+actionsParser.add_argument("--addReplicaMember",dest='action', action='store_const'
+        ,const=addReplicaMember
+        ,help='Add member to existing replica set')
 
 
 optionsParser = parser.add_argument_group('options')
@@ -198,6 +299,9 @@ optionsParser.add_argument("--rsHosts"
         ,required=False)
 optionsParser.add_argument("--rsTemplate"
         ,help='Input file containing JSON template for replica set config'
+        ,required=False)
+optionsParser.add_argument("--hostPort"
+        ,help='host:port of the replica member to remove'
         ,required=False)
 
 
